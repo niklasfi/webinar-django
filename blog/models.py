e1893760446569bc50a976b70236570f424062ae
Niklas Fischer <niklas@niklasfi.de>
2015-02-22 12:47:36 +0100

    add Comment model class

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,18 @@ class Post(models.Model):
         title=self.title,
         published=self.published.date()
     )
+
+class Comment(models.Model):
+  post = models.ForeignKey('Post')
+  nick = models.CharField(max_length=32)
+  body = models.TextField()
+  published = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ['published']
+
+  def __str__(self):
+    return "{nick}: {body}".format(
+        nick=self.nick,
+        body=self.body
+    )
